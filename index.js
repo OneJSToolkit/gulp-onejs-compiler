7ddb901d4d7c0092ff4aa95fbb57890dd656230e
David Zearing <david@davids-mbp-2.home.network>
2014-10-08 23:55:10 -0700

    adding try/catch. bumping ver.

--- index.js
+++ index.js
@@ -8,48 +8,55 @@ var typeScriptViewModelGenerator = require('onejs-compiler').TypeScriptViewModel
 
 module.exports = function(options) {
 
-  options = options || {
-    typeScriptViewFileFormat: '{{templateName}}.ts',
-    typeScriptViewModelFileFormat: 'I{{templateName}}Model.ts'
-  };
+    options = options || {
+        typeScriptViewFileFormat: '{{templateName}}.ts',
+        typeScriptViewModelFileFormat: 'I{{templateName}}Model.ts'
+    };
 
-  function getFileName(nameMatch, templateName) {
-    return nameMatch.replace('{{templateName}}', templateName);
-  }
-
-  function generate(generatorType, templateContent, fileNameFormat, file) {
-      var generator = new generatorType();
-      var outputFile = file.clone();
-
-      outputFile.contents = new Buffer(generator.generate(templateContent));
-      outputFile.path = file.path.replace(path.basename(file.path), getFileName(fileNameFormat, generator.template.name));
-
-      return outputFile;
-  }
-
-  var stream = through.obj(function(file, enc, callback) {
-    if (file.isNull() || !file.contents || path.extname(file.path).toLowerCase() !== '.html') {
-      this.push(file);
-      callback();
-      return;
+    function getFileName(nameMatch, templateName) {
+        return nameMatch.replace('{{templateName}}', templateName);
     }
 
-    var fileContent = file.contents.toString('utf8');
-    var outputFile;
+    function generate(generatorType, templateContent, fileNameFormat, file) {
+        var generator = new generatorType();
+        var outputFile = file.clone();
 
-    // Build typescript view class.
-    if (options.typeScriptViewFileFormat) {
-      this.push(generate(typeScriptGenerator, fileContent, options.typeScriptViewFileFormat, file));
+        try {
+            outputFile.contents = new Buffer(generator.generate(templateContent));
+            outputFile.path = file.path.replace(path.basename(file.path), getFileName(fileNameFormat, generator.template.name));
+
+        } catch (e) {
+            throw new gutil.PluginError({
+                plugin: 'gulp-onejs-compiler',
+                message: 'Error parsing template: ' + file.path + '\n' + e
+            });
+        }
+
+        return outputFile;
     }
 
-    // Build typescript view model interface.
-    if (options.typeScriptViewModelFileFormat) {
-      this.push(generate(typeScriptViewModelGenerator, fileContent, options.typeScriptViewModelFileFormat, file));
-    }
+    var stream = through.obj(function(file, enc, callback) {
+        if (file.isNull() || !file.contents || path.extname(file.path).toLowerCase() !== '.html') {
+            this.push(file);
+            callback();
+            return;
+        }
 
-    callback();
-  });
+        var fileContent = file.contents.toString('utf8');
+        var outputFile;
 
-  return stream;
+        // Build typescript view class.
+        if (options.typeScriptViewFileFormat) {
+            this.push(generate(typeScriptGenerator, fileContent, options.typeScriptViewFileFormat, file));
+        }
+
+        // Build typescript view model interface.
+        if (options.typeScriptViewModelFileFormat) {
+            this.push(generate(typeScriptViewModelGenerator, fileContent, options.typeScriptViewModelFileFormat, file));
+        }
+
+        callback();
+    });
+
+    return stream;
 };
-
